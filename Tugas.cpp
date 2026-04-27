9becdb2902c80069922cbd02a1c5157ce77bd76d
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-04-27 17:58:59 +0700

    tugas

--- Tugas.cpp
+++ Tugas.cpp
@@ -1,8 +1,88 @@
-// buatlah sebuah program yang berisi 2 class dimana terdapat class layang - layang, Belag ketupat.
-// disetiap class nya berisikan luas dan keliling serta input dan output
-// setiap variable memiliki akses private, Bangundatar Belah ketupat merupakan friend function keliling dari layar
-// semua class hanya dapat di akses menggunakan object
-
-
 #include <iostream>
-#include <string>
+using namespace std;
+
+// Deklarasi class
+class BelahKetupat;
+class LayangLayang
+{
+private:
+    double diagonal1, diagonal2, sisi1, sisi2;
+
+public:
+    void inputLayang();
+    void luasLayang();
+    friend void keliling(LayangLayang &a, BelahKetupat &b);
+};
+
+// Class Belah Ketupat
+class BelahKetupat
+{
+private:
+    double diagonal1, diagonal2, sisi;
+
+public:
+    void inputBelah();
+    void luasBelah();
+    friend void keliling(LayangLayang &a, BelahKetupat &b);
+};
+
+// Input Layang-Layang
+void LayangLayang::inputLayang()
+{
+    cout << "Input diagonal 1 Layang-Layang : ";
+    cin >> diagonal1;
+    cout << "Input diagonal 2 Layang-Layang : ";
+    cin >> diagonal2;
+    cout << "Input sisi 1 Layang-Layang     : ";
+    cin >> sisi1;
+    cout << "Input sisi 2 Layang-Layang     : ";
+    cin >> sisi2;
+}
+
+// Luas Layang-Layang
+void LayangLayang::luasLayang()
+{
+    cout << "Luas Layang-Layang : " << 0.5 * diagonal1 * diagonal2 << endl;
+}
+
+// Input Belah Ketupat
+void BelahKetupat::inputBelah()
+{
+    cout << "\nInput diagonal 1 Belah Ketupat : ";
+    cin >> diagonal1;
+    cout << "Input diagonal 2 Belah Ketupat : ";
+    cin >> diagonal2;
+    cout << "Input sisi Belah Ketupat       : ";
+    cin >> sisi;
+}
+
+// Luas Belah Ketupat
+void BelahKetupat::luasBelah()
+{
+    cout << "Luas Belah Ketupat : " << 0.5 * diagonal1 * diagonal2 << endl;
+}
+
+// Friend Function Keliling
+void keliling(LayangLayang &a, BelahKetupat &b)
+{
+    cout << "Keliling Layang-Layang : " << 2 * (a.sisi1 + a.sisi2) << endl;
+    cout << "Keliling Belah Ketupat : " << 4 * b.sisi << endl;
+}
+
+// Main Program
+int main()
+{
+    LayangLayang layang;
+    BelahKetupat belah;
+
+    layang.inputLayang();
+    belah.inputBelah();
+
+    cout << "\n=== Hasil ===" << endl;
+    layang.luasLayang();
+    belah.luasBelah();
+
+    keliling(layang, belah);
+
+    return 0;
+}
